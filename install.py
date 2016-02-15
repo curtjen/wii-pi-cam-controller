4d6b16743caa8bd74dd252f119aea8522f178652
Curtis Jensen <jensen.cletus@gmail.com>
2016-02-14 22:46:12 -0700

    Added install.py .

---
+++ install.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import os
+
+# Install gphoto2
+print "Installing gphoto2..."
+os.system("wget https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/gphoto2-updater.sh && chmod +x gphoto2-updater.sh && sudo ./gphoto2-updater.sh")
+
+# Change 'shutdown' command permissions
+print "Changing permissions to 'shutdown' command for Wiimote use..."
+os.system("sudo chmod a+s /sbin/shutdown")
